f9d9ff5dc622b0e045b8155f96640f38da152851
Casa <>
2018-11-25 16:20:28 -0500

    Se incluyen las colisiones de los botones de pared, se completa el funcionamiento de sus mecánicas y se implementan ss códigos para adaptarse al sistema de prefabs anidados

---
+++ Assets/Scripts/Mecanicas/RevisionTrigger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RevisionTrigger : MonoBehaviour
+{
+
+    public bool EstaEnTrigger = false;
+
+    private void OnTriggerEnter(Collider Jugador)
+    {
+
+        if (Jugador.CompareTag("Player"))
+        {
+
+            EstaEnTrigger = true;
+
+        }
+
+    }
+
+    private void OnTriggerExit(Collider Jugador)
+    {
+
+        if (Jugador.CompareTag("Player"))
+        {
+
+            EstaEnTrigger = false;
+
+        }
+
+
+    }
+}
